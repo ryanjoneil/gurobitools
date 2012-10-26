3260f556585dcd89a99379920f5d3ecaa9bd93cc
Ryan J. O'Neil <ryanjoneil@gmail.com>
2012-10-26 17:32:44 -0400

    almost have the command line version working (though it sure is ugly)

--- src/gurobitools/model.py
+++ src/gurobitools/model.py
@@ -36,3 +36,46 @@ class ProxyModel(Model):
                 return setattr(ProxyModel.PROXY[self], attr, value)
             except AttributeError:
                 raise e
+
+def copy_model(model, to_class):
+    '''Takes in a Gurobi Model instance and copies it to a subtype.'''
+    # TODO: this only does linear constraints right now..
+    # TODO: there *has* to be a better way to do this
+    m = to_class()
+
+    # Add all variables and their bounds.
+    var_map = {}
+    for v in model.getVars():
+        v2 = m.addVar(
+            lb=v.lb, ub=v.ub, obj=v.obj, vtype=v.vtype, name=v.varName
+        )
+        var_map[v] = v2
+
+    m.update()
+
+    # Add all constraints, constructing expressions for them.
+    for c in model.getConstrs():
+        expr = 0
+        for v in model.getVars():
+            coeff = model.getCoeff(c, v)
+            if coeff:
+                expr += coeff * var_map[v]
+
+        if expr:
+            if c.sense == '<':
+                m.addConstr(expr <= c.rhs)
+            elif c.sense == '>':
+                m.addConstr(expr >= c.rhs)
+            else:
+                m.addConstr(expr == c.rhs)
+
+    # And finally add the objective function.
+    obj = model.getObjective()
+    expr = obj.getConstant()
+    for i in xrange(obj.size()):
+        v = obj.getVar(i)
+        coeff = obj.getCoeff(i)
+        expr += coeff * var_map[v]
+
+    m.setObjective(expr)
+    return m
